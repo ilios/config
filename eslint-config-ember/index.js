eff688496be476ca6e1a970899ec308e1ea1d0ed
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-02-20 23:18:13 -0800

    Adopt all recommended ember rules
    ... and a few extra ones that have an easy fix of seem like a good idea.

--- eslint-config-ember/index.js
+++ eslint-config-ember/index.js
@@ -10,19 +10,16 @@ module.exports = {
     browser: true
   },
   rules: {
-    'ember/new-module-imports': 2,
-    'ember/no-old-shims': 2,
-    'ember/closure-actions': 0,
-    'ember/alias-model-in-controller': 0,
-    'ember/use-ember-get-and-set': 0,
-    'ember/named-functions-in-promises': 0,
-    'ember/no-capital-letters-in-routes': 0,
-    'ember/routes-segments-snake-case': 0,
-    'ember/avoid-leaking-state-in-components': 0,
+    'ember/no-empty-attrs': 2,
+    'ember/no-jquery': 1,
+    'ember/no-new-mixins': 2,
+    'ember/use-ember-get-and-set': 2,
+    'ember/no-unnecessary-route-path-option': 2,
+    'ember/no-test-and-then': 2,
+    'ember/no-test-import-export': 2,
     'ember/order-in-components': 2,
     'ember/order-in-controllers': 2,
     'ember/order-in-routes': 2,
-    'ember/use-brace-expansion': 0,
   },
   overrides: [
     // node files
